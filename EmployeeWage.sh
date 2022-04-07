be32906804d0b72e8f653f06e151792be8a9caed
Arshad <mohammedarshad183@gmail.com>
2022-04-07 14:39:44 +0530

    Refactored: Calculating Wages per Month

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,17 +1,27 @@
 #!/bin/bash -x
 IsFullTime=1;
 IsPartTime=2;
-IsRandomCheck=$((RANDOM%3));
 IsWorkingHrs=0;
-PerHr=100;
+PerHr=20;
+TotalSalary=0;
+NoOfWorkingDays=20;
+Present=0;
+for (( day=1; day<=$NoOfWorkingDays; day++ ));
+do
+        IsRandomCheck=$((RANDOM%3));
 case $IsRandomCheck in
         $IsFullTime)
-                IsWorkingHrs=8;;
+                IsWorkingHrs=8;
+                Present=$(($Present+1));;
         $IsPartTime)
-                IsWorkingHrs=4;;
+                IsWorkingHrs=4;
+                ((Present+1));;
         *)
                 IsWorkingHrs=0;;
 esac
-salary=$(($PerHr*$IsWorkingHrs));
-echo $salary;
+Salary=$(($IsWorkingHrs*$PerHr))
+TotalSalary=$(($Salary+$TotalSalary));
+done
+echo Total_Salary ":" $TotalSalary;
+echo Number_Of_Days_Present ":" $Present;
 
