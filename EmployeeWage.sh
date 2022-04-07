d2d6ec4230c01dcef4dea8f9ce1d7107cf7a1b3b
Arshad <mohammedarshad183@gmail.com>
2022-04-07 14:17:42 +0530

    Refactored: Add Part time Employee & Wage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -x
 IsFullTime=1;
-IsRandomCheck=$((RANDOM%2));
+IsPartTime=2
+IsRandomCheck=$((RANDOM%3));
 IsWorkingHrs=0;
 PerHr=20;
 if [ $IsFullTime -eq $IsRandomCheck ];
 then
         IsWorkingHrs=8;
+elif [ $IsPartTime -eq $IsRandomCheck ];
+then
+	IsWorkingHrs=4;
 else
         IsWorkingHrs=0;
 fi
