7721e15191b971ce304e975eff02912106077fe1
Arshad <mohammedarshad183@gmail.com>
2022-04-07 14:26:17 +0530

    Refactored: Solving using Switch case statement

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,18 +1,17 @@
 #!/bin/bash -x
 IsFullTime=1;
-IsPartTime=2
+IsPartTime=2;
 IsRandomCheck=$((RANDOM%3));
 IsWorkingHrs=0;
-PerHr=20;
-if [ $IsFullTime -eq $IsRandomCheck ];
-then
-        IsWorkingHrs=8;
-elif [ $IsPartTime -eq $IsRandomCheck ];
-then
-	IsWorkingHrs=4;
-else
-        IsWorkingHrs=0;
-fi
-Salary=$(($IsWorkingHrs*$PerHr));
-echo $Salary;
+PerHr=100;
+case $IsRandomCheck in
+        $IsFullTime)
+                IsWorkingHrs=8;;
+        $IsPartTime)
+                IsWorkingHrs=4;;
+        *)
+                IsWorkingHrs=0;;
+esac
+salary=$(($PerHr*$IsWorkingHrs));
+echo $salary;
 
